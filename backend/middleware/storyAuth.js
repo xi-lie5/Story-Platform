76191d5ab1e557341189eba7b87e590d2bfddb0a
Requiem <khyunchan@foxmail.com>
2025-11-21 16:18:55 +0800

    零接口改动
    1.story.author 可能是 ObjectId 或字符串，统一用 .equals()增加安全性
    2.加一层「故事已软删除」判断

--- backend/middleware/storyAuth.js
+++ backend/middleware/storyAuth.js
@@ -1,32 +1,30 @@
+// middleware/storyAuth.js
 const Story = require('../models/Story');
-const { errorFormat } = require('../utils/errorFormat');
+
+// 集中配置
+const ERR = {
+  BAD_REQUEST: (msg, code) => require('../utils/errorFormat')(400, msg, [], code),
+  NOT_FOUND:   (msg, code) => require('../utils/errorFormat')(404, msg, [], code),
+  FORBIDDEN:   (msg, code) => require('../utils/errorFormat')(403, msg, [], code),
+};
+
+const CODES = {
+  STORY_ID_REQUIRED: 10010,
+  STORY_NOT_EXIST:   10011,
+  NOT_AUTHOR:        10005,
+};
 
 module.exports = async function storyAuth(req, res, next) {
-  try {
-    const { storyId } = req.params;
-    
-    if (!storyId) {
-      return next(errorFormat(400, '故事ID必填', [], 10010));
-    }
-    
-    // 查找故事
-    const story = await Story.findById(storyId);
-    
-    if (!story) {
-      return next(errorFormat(404, '故事不存在', [], 10011));
-    }
-    
-    // 检查用户权限：只有故事作者可以访问
-    if (story.author.toString() !== req.user.id) {
-      return next(errorFormat(403, '无权限访问此故事', [], 10005));
-    }
-    
-    // 将故事信息附加到请求对象
-    req.story = story;
-    
-    next();
-  } catch (error) {
-    console.error('StoryAuth中间件错误:', error);
-    return next(errorFormat(500, '权限验证失败', [], 10012));
+  const { storyId } = req.params;
+  if (!storyId) return next(ERR.BAD_REQUEST('故事ID必填', CODES.STORY_ID_REQUIRED));
+
+  const story = await Story.findById(storyId);        // 如有软删除：.findOne({ _id: storyId, deletedAt: null })
+  if (!story) return next(ERR.NOT_FOUND('故事不存在', CODES.STORY_NOT_EXIST));
+
+  if (!story.author.equals(req.user.id)) {            // 兼容 ObjectId 与字符串
+    return next(ERR.FORBIDDEN('无权限访问此故事', CODES.NOT_AUTHOR));
   }
+
+  req.story = story;
+  next();
 };
